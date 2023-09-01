b96ad4f028cbafa7d335ee78f66598e95b9ca1b9
shade2903 <nighshade2903@gmail.com>
2023-09-01 16:21:49 +0300

    fix service test

--- module-service/src/test/java/com/mjc/school/service/AuthorServiceTest.java
+++ module-service/src/test/java/com/mjc/school/service/AuthorServiceTest.java
@@ -5,8 +5,10 @@ package com.mjc.school.service;
 import com.mjc.school.repository.impl.AuthorRepository;
 import com.mjc.school.repository.model.impl.AuthorModel;
 import com.mjc.school.repository.model.impl.NewsModel;
+import com.mjc.school.service.dto.AuthorDtoRequest;
 import com.mjc.school.service.dto.AuthorDtoResponse;
 import com.mjc.school.service.impl.AuthorService;
+import com.mjc.school.service.mapper.AuthorMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -25,8 +27,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
+    private final Long EXPECTED_ID = 4l;
+
+    private final String EXPECTED_NAME = "TestAuthorName";
     private final  List<AuthorModel> authorList = new ArrayList<>();
     private final List<NewsModel> newsList = new ArrayList<>();
+
+    private AuthorDtoRequest authorDtoRequest;
+
+    private AuthorModel authorModel;
 
     @Mock
     private AuthorRepository authorRepository;
@@ -36,6 +45,9 @@ public class AuthorServiceTest {
 
     @BeforeEach
     void init(){
+        authorDtoRequest = new AuthorDtoRequest(4L,"TestAuthorName");
+        authorModel = new AuthorModel(4L, "TestAuthorName", LocalDateTime.now(), LocalDateTime.now())
+
         authorList.add(new AuthorModel(1L,"Maik Herst", LocalDateTime.now(), LocalDateTime.now()));
         authorList.add(new AuthorModel(2L,"Jodi Foster", LocalDateTime.now(), LocalDateTime.now()));
         authorList.add(new AuthorModel(3L,"Henry Cavil", LocalDateTime.now(), LocalDateTime.now()));
@@ -50,9 +62,13 @@ public class AuthorServiceTest {
         assertEquals(authorList.size(), authorDtoResponses.size());
     }
 
-
-
-
+//    @Test
+//    @DisplayName("test method create()")
+//    void testCreate(){
+//        Mockito.when(authorRepository.create(authorModel))
+//                .thenReturn(AuthorMapper.INSTANCE.authorToDtoResponse(authorModel));
+//        AuthorDtoResponse authorDtoResponse = authorRepository.create(authorModel)
+//    }
 
 
 }
